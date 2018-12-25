4712b163f153ebfac8b4323a40c28437e5139713
Hari kiran <40531762+A2hari@users.noreply.github.com>
2018-12-25 12:49:02 +0530

    adding comments for better readability of the code

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -1,7 +1,7 @@
-import tweepy
-import time
+import tweepy           #library  for building apps for twitter
+import time            #library for controling the sleep of the message
 
-CONSUMER_KEY ="Enter your api key"
+CONSUMER_KEY ="Enter your api key"   
 CONSUMER_SECRET = "Enter your api secret key"
 ACCESS_KEY ="Enter your access key"
 ACCESS_SECRET="Enter your access secret key"
@@ -10,7 +10,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-file = "last_tweet_id.txt"
+file = "last_tweet_id.txt"           #linking the file which stores the last tweet id
 
 def read_last_id(file):
         file_read = open(file,'r')
@@ -36,6 +36,6 @@ def reply_to_tweets():
               print("found Hello world!\nWaving you back user ......",flush=True)
               api.update_status("@"+mention.user.screen_name+" "+"Hello world waving back to you !",mention.id)
 
-while True:
+while True:                   #infinite loop
     reply_to_tweets()
-    time.sleep(6)
+    time.sleep(6)               
